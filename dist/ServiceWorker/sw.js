31248f3dec2812f18eaec09607c6591ac943dc89
= <betowithowsky@gmail.com>
2018-11-29 03:54:14 -0200

    Configurando o ServiceWorker 1v

--- dist/ServiceWorker/sw.js
+++ dist/ServiceWorker/sw.js
@@ -29,3 +29,17 @@ if ('serviceWorker' in navigator) {
         })
     );
   });
+
+  self.addEventListener('fetch', function(event) {
+    event.respondWith(
+      caches.match(event.request)
+        .then(function(response) {
+          // Cache hit - return response
+          if (response) {
+            return response;
+          }
+          return fetch(event.request);
+        }
+      )
+    );
+  });
